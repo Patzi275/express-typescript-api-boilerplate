248046956fa401a69f735a5bfca59c225b2d1b3c
Patrick Zocli <patrickzocli2021@gmail.com>
2024-04-13 22:06:44 +0100

    Update db & conf conf files

---
+++ src/config/db.config.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+
+config();
+
+export default {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DATABASE: process.env.DB_NAME,
+};
