47ba2536bd07fa64ba15dbfed8a351f7c80ae285
Patrick Zocli <patrickzocli2021@gmail.com>
2024-04-16 22:03:55 +0100

    Update server init function

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,16 @@ import app from './app';
 import { port } from './config/server.config';
 import db from './services/db';
 
-const initApp = async () => {
-    console.log('Database connection...');
-
-    app.listen(port || 3000, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-
+const initApp = async () => {    
     try {
+        console.log('Database connection...');
         await db.authenticate();
+        await db.sync({force: true});
         console.log("Database connection has been established successfully.");
+
+        app.listen(port || 3000, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
     } catch (error) {
         console.error("Unable to connect to the database:", error);
     }
